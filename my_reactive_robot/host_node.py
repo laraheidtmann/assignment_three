5649fd5a0df5b50ee32fdf2447e84e9d8632ad26
Leonhard Haselbek <up202501695@edu.fe.up.pt>
2025-10-08 14:19:58 +0100

    Initial commit: create ROS package

---
+++ my_reactive_robot/host_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from my_reactive_robot.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_reactive_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lhase',
+    maintainer_email='up202501695@edu.fe.up.pt',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'host_node = my_reactive_robot.host_node:main'
+        ],
+    },
+)
