b3816a3f7646a5f5d071d2fc9946166400d06a5b
Leonhard Haselbek <up202501695@edu.fe.up.pt>
2025-10-08 14:42:47 +0100

    Add basic robot (from tutorial).

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 from setuptools import find_packages, setup
 
 package_name = 'my_reactive_robot'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf']))
+data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='lhase',
-    maintainer_email='up202501695@edu.fe.up.pt',
+    maintainer='user',
+    maintainer_email='user.name@mail.com',
     description='TODO: Package description',
-    license='Apache-2.0',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    license='TODO: License declaration',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'host_node = my_reactive_robot.host_node:main'
+            'host_node = my_reactive_robot.host_node:main',
         ],
     },
 )
